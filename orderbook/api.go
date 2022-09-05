bb2ff47008e5f1bca6daf6921105b99071125e13
Innocent Zvovuranda <innocent@luno.com>
2022-09-05 09:14:34 +0200

    Add client implementation

--- orderbook/api.go
+++ orderbook/api.go
@@ -1,4 +1,13 @@
 package orderbook
 
-type OrderbookClient interface {
+import (
+	"context"
+	"spotob/orderbook/models"
+)
+
+type OrderBookClient interface {
+	ProcessLimitOrder(ctx context.Context, req models.LimitOrder) (*models.LimitOrderResponse, error)
+	ProcessMarketOrder(ctx context.Context, req models.MarketOrderRequest) (*models.MarketOrderResponse, error)
+	CancelOrder(ctx context.Context, Id string) (*models.Order, error)
+	Depth(ctx context.Context) (*models.OrderBookDepth, error)
 }
