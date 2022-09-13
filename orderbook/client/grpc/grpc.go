50842ce48d6a67f442b5d1ecf185e4f82a98a558
Innocent Zvovuranda <innocent@luno.com>
2022-09-13 08:52:16 +0200

    added a grpc client

---
+++ orderbook/client/grpc/grpc.go
@@ -0,0 +1,144 @@
+package grpc
+
+import (
+	"context"
+	"github.com/muzykantov/orderbook"
+	"github.com/shopspring/decimal"
+	"google.golang.org/grpc"
+	"log"
+	ob "spotob/orderbook"
+	"spotob/orderbook/models"
+	pb "spotob/orderbook/orderbookpb"
+)
+
+type orderBookgRPCClient struct {
+	Ob       *orderbook.OrderBook
+	RpcConn  *grpc.ClientConn
+	ObClient pb.OrderBookgRPCServiceClient
+}
+
+func (c *orderBookgRPCClient) ProcessLimitOrder(ctx context.Context, req models.LimitOrder) (*models.LimitOrderResponse, error) {
+	//Todo : Add some validation
+	res, err := c.ObClient.ProcessLimitOrder(ctx, &pb.LimitOrderRequest{
+		Side:     req.Side,
+		OrderId:  req.OrderId,
+		Quantity: req.Quantity.String(),
+		Price:    req.Price.String(),
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	orders, err := pb.OrderspbToOrders(res.Orders)
+	if err != nil {
+		return nil, err
+	}
+
+	partial, err := pb.OrderpbToOrder(res.Partial)
+	if err != nil {
+		return nil, err
+	}
+
+	pqp, err := decimal.NewFromString(res.PartialQuantityProcessed)
+	if err != nil {
+		return nil, err
+	}
+
+	return &models.LimitOrderResponse{
+		Done:                     orders,
+		Partial:                  partial,
+		PartialQuantityProcessed: pqp,
+	}, nil
+}
+
+func (c *orderBookgRPCClient) ProcessMarketOrder(ctx context.Context, req models.MarketOrderRequest) (*models.MarketOrderResponse, error) {
+	res, err := c.ObClient.ProcessMarketOrder(ctx, &pb.MarketOrderRequest{
+		Side:     req.Side,
+		Quantity: req.Quantity.String(),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	orders, err := pb.OrderspbToOrders(res.Done)
+	if err != nil {
+		return nil, err
+	}
+
+	partial, err := pb.OrderpbToOrder(res.Partial)
+	if err != nil {
+		return nil, err
+	}
+
+	pqp, err := decimal.NewFromString(res.PartialQuantityProcessed)
+	if err != nil {
+		return nil, err
+	}
+
+	ql, err := decimal.NewFromString(res.QuantityLeft)
+	if err != nil {
+		return nil, err
+	}
+
+	return &models.MarketOrderResponse{
+		Done:                     orders,
+		Partial:                  partial,
+		PartialQuantityProcessed: pqp,
+		QuantityLeft:             ql,
+	}, nil
+}
+
+func (c *orderBookgRPCClient) CancelOrder(ctx context.Context, Id string) (*models.Order, error) {
+	res, err := c.ObClient.CancelOrder(ctx, &pb.CancelOrderRequest{
+		Id: Id,
+	})
+
+	order, err := pb.OrderpbToOrder(res.Order)
+	if err != nil {
+		return nil, err
+	}
+
+	return &models.Order{
+		Side:      int32(order.Side()),
+		Id:        order.ID(),
+		Timestamp: order.Time(),
+		Quantity:  order.Quantity(),
+		Price:     order.Price(),
+	}, nil
+}
+
+func (c *orderBookgRPCClient) Depth(ctx context.Context) (*models.OrderBookDepth, error) {
+	res, err := c.ObClient.Depth(ctx, &pb.Empty{})
+	if err != nil {
+		return nil, err
+	}
+
+	bids, err := pb.FromPriceLevelpbs(res.Bids)
+	if err != nil {
+		return nil, err
+	}
+	asks, err := pb.FromPriceLevelpbs(res.Asks)
+	if err != nil {
+		return nil, err
+	}
+
+	return &models.OrderBookDepth{
+		Bids: bids,
+		Asks: asks,
+	}, nil
+}
+
+func NewOrderBookClient(connStri string, ob *orderbook.OrderBook) (ob.OrderBookClient, error) {
+	conn, err := grpc.Dial(connStri, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+		return nil, err
+	}
+
+	obc := pb.NewOrderBookgRPCServiceClient(conn)
+	return &orderBookgRPCClient{
+		RpcConn:  conn,
+		ObClient: obc,
+		Ob:       ob,
+	}, nil
+}
--- orderbook/orderbookpb/util.go
+++ orderbook/orderbookpb/util.go
@@ -1,7 +1,9 @@
 package orderbookpb
 
 import (
+	"errors"
 	ob "github.com/muzykantov/orderbook"
+	"github.com/shopspring/decimal"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
@@ -13,11 +15,11 @@ func ConvertOrderToOrderpb(order *ob.Order) *Order {
 
 	time := timestamppb.New(order.Time())
 	return &Order{
-		Side:                 int32(order.Side()),
-		Id:                   order.ID(),
-		Timestamp:            time,
-		Quantity:             order.Quantity().String(),
-		Price:                order.Price().String(),
+		Side:      int32(order.Side()),
+		Id:        order.ID(),
+		Timestamp: time,
+		Quantity:  order.Quantity().String(),
+		Price:     order.Price().String(),
 	}
 }
 
@@ -30,17 +32,82 @@ func ConvertOrdersToOrderpb(orders []*ob.Order) []*Order {
 	return orderList
 }
 
+func OrderpbToOrder(o *Order) (*ob.Order, error) {
+	if o == nil {
+		return nil, errors.New("cannot convert an empty order")
+	}
+	q, err := decimal.NewFromString(o.Quantity)
+	if err != nil {
+		return nil, err
+	}
+
+	p, err := decimal.NewFromString(o.Price)
+	if err != nil {
+		return nil, err
+	}
+
+	ts := o.Timestamp.AsTime()
+	side := ob.Side(int(o.Side))
+	return ob.NewOrder(o.Id, side, q, p, ts), nil
+}
+
+func OrderspbToOrders(ods []*Order) ([]*ob.Order, error) {
+	var orders []*ob.Order
+	for _, od := range ods {
+		order, err := OrderpbToOrder(od)
+		if err != nil {
+			return nil, err
+		}
+		orders = append(orders, order)
+	}
+	return orders, nil
+}
+
 func ToPriceLevelpb(pl *ob.PriceLevel) *PriceLevel {
 	return &PriceLevel{
-		Price: pl.Price.String(),
+		Price:    pl.Price.String(),
 		Quantity: pl.Quantity.String(),
 	}
 }
 
 func ToPriceLevelpbs(pls []*ob.PriceLevel) []*PriceLevel {
 	var priceLevels []*PriceLevel
-	for _,pl := range pls {
+	for _, pl := range pls {
 		priceLevels = append(priceLevels, ToPriceLevelpb(pl))
 	}
 	return priceLevels
+}
+
+func FromPriceLevelpb(pl *PriceLevel) (*ob.PriceLevel, error) {
+	if pl == nil {
+		return nil, errors.New("cannot convert an empty price level")
+	}
+
+	p, err := decimal.NewFromString(pl.Price)
+	if err != nil {
+		return nil, err
+	}
+
+	q, err := decimal.NewFromString(pl.Quantity)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ob.PriceLevel{
+		Price:    p,
+		Quantity: q,
+	}, nil
+}
+
+func FromPriceLevelpbs(pls []*PriceLevel) ([]*ob.PriceLevel, error) {
+	var priceLevels []*ob.PriceLevel
+	for _, pl := range pls {
+
+		obpl, err := FromPriceLevelpb(pl)
+		if err != nil {
+			return nil, err
+		}
+		priceLevels = append(priceLevels, obpl)
+	}
+	return priceLevels, nil
 }
